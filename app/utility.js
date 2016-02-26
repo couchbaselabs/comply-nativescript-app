8a6d38188065ced060c560d0fd4b9b5f533ea449
Nic Raboy <nraboy@gmail.com>
2016-02-26 09:07:58 -0800

    the http module returns a promise so no need to wrap it

--- app/utility.js
+++ app/utility.js
@@ -5,17 +5,11 @@ exports.makePostRequest = function(url, params, body) {
     if(params && params.length > 0) {
         fullUrl = fullUrl + "/" + params.join("/");
     }
-    return new Promise((resolve, reject) => {
-        httpModule.request({
-            url: fullUrl,
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            content: JSON.stringify(body)
-        }).then(function(response) {
-            resolve(response.content.toJSON());
-        }, function(error) {
-            reject(error);
-        });
+    return httpModule.request({
+        url: fullUrl,
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify(body)
     });
 }
 
@@ -24,11 +18,5 @@ exports.makeGetRequest = function(url, params) {
     if(params && params.length > 0) {
         fullUrl = fullUrl + "/" + params.join("/");
     }
-    return new Promise((resolve, reject) => {
-        httpModule.getJSON(fullUrl).then(function(response) {
-            resolve(response);
-        }, function(error) {
-            reject(error);
-        });
-    });
+    return httpModule.getJSON(fullUrl);
 }
